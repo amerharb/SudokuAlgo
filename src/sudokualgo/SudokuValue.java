b54c7bbb7eb2ac787758dacf5b919e0c47f9d010
amerharb <amer.harb@gmail.com>
2016-04-16 13:00:36 +0200

    add sceleton classes

---
+++ src/sudokualgo/SudokuValue.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudokualgo;
+
+/**
+ *
+ * @author Amer
+ */
+public enum SudokuValue
+{
+
+    SV_1, SV_2, SV_3, SV_4, SV_5, SV_6, SV_7, SV_8, SV_9
+}
